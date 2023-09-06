699f8e21a22969a6ecf38ea1ce6587c54ea1490e
Kiruthika60577 <138123688+Kiruthika60577@users.noreply.github.com>
2023-09-06 17:23:33 +0200

    Update streamlitapp.py

--- streamlitapp.py
+++ streamlitapp.py
@@ -13,28 +13,21 @@ st.text("Enjoy personalized movie recommendations!")
 # Input for User's Preferred Genre
 genre = st.text_input("Enter your preferred movie genre")
 
-# File uploader widget for uploading a dataset
-uploaded_file = st.file_uploader("Upload your movie dataset (CSV or Excel)", type=["csv", "xlsx"])
+# Load movies.csv from GitHub
+movies_url = 'https://raw.githubusercontent.com/Kiruthika60577/Kiruthika60577/main/movies.csv'
+movies_df = pd.read_csv(movies_url)
 
-# Load movies.csv from Google Drive
-movies_url = 'https://drive.google.com/file/d/1fQ4E2q3LKng0ShMswV8kExZJtzfabn-P/view?usp=sharing'
-movies_path = 'https://drive.google.com/uc?export=download&id=' + movies_url.split('/')[-2]
-movies_df = pd.read_csv(movies_path)
+# Load ratings.csv from GitHub
+ratings_url = 'https://raw.githubusercontent.com/Kiruthika60577/Kiruthika60577/main/ratings.csv'
+ratings_df = pd.read_csv(ratings_url)
 
-# Load ratings.csv from Google Drive
-ratings_url = 'https://drive.google.com/file/d/1cOW6sRWehqhw8936fFFuSgvkVxVlgyyh/view?usp=sharing'
-ratings_path = 'https://drive.google.com/uc?export=download&id=' + ratings_url.split('/')[-2]
-ratings_df = pd.read_csv(ratings_path)
+# Load links.csv from GitHub
+links_url = 'https://raw.githubusercontent.com/Kiruthika60577/Kiruthika60577/main/links.csv'
+links_df = pd.read_csv(links_url)
 
-# Load links.csv from Google Drive
-links_url = 'https://drive.google.com/file/d/1aEracXXZf9HG5U-o_Mw3KX8aaFveiVvR/view?usp=sharing'
-links_path = 'https://drive.google.com/uc?export=download&id=' + links_url.split('/')[-2]
-links_df = pd.read_csv(links_path)
-
-# Load tags.csv from Google Drive
-tags_url = 'https://drive.google.com/file/d/1Fc9sf1t5yOR6rgtn-2-EMT5YKw5mReVI/view?usp=sharing'
-tags_path = 'https://drive.google.com/uc?export=download&id=' + tags_url.split('/')[-2]
-tags_df = pd.read_csv(tags_path)
+# Load tags.csv from GitHub
+tags_url = 'https://raw.githubusercontent.com/Kiruthika60577/Kiruthika60577/main/tags.csv'
+tags_df = pd.read_csv(tags_url)
 
 # Display recommended movies based on genre
 if genre:
