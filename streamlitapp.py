397cbbd03b7dcfb0a64d73be91fe343d7af57176
Kiruthika60577 <138123688+Kiruthika60577@users.noreply.github.com>
2023-09-06 17:50:03 +0200

    Update streamlitapp.py

--- streamlitapp.py
+++ streamlitapp.py
@@ -1,43 +1,9 @@
-import streamlit as st
 import pandas as pd
 
-# Title of your Streamlit app
-st.title("WBSFlix - Movie Recommendations")
+# Define the path to your CSV file
+csv_file_path = 'movies.csv'
 
-# Header
-st.header("Movie Recommendations")
+# Load the data using pandas
+df = pd.read_csv(/content/Kiruthika60577/movies.csv)
 
-# Description
-st.text("Enjoy personalized movie recommendations!")
-
-# Input for User's Preferred Genre
-genre = st.text_input("Enter your preferred movie genre")
-
-# Load movies.csv from GitHub
-movies_url = 'https://raw.githubusercontent.com/Kiruthika60577/Kiruthika60577/main/movies.csv'
-movies_df = pd.read_csv(movies_url)
-
-# Load ratings.csv from GitHub
-ratings_url = 'https://raw.githubusercontent.com/Kiruthika60577/Kiruthika60577/main/ratings.csv'
-ratings_df = pd.read_csv(ratings_url)
-
-# Check column names in both DataFrames
-st.write("Movies DataFrame Columns:", movies_df.columns)
-st.write("Ratings DataFrame Columns:", ratings_df.columns)
-
-# Identify the correct common column for merging (replace 'CommonColumn' with the actual common column name)
-common_column = 'CommonColumn'  # Replace with the correct column name
-
-# Merge movies_df and ratings_df based on the common column
-merged_df = movies_df.merge(ratings_df, on=common_column, how='inner')
-
-# Display recommended movies based on genre
-if genre:
-    genre = genre.lower()
-    recommended_movies = merged_df[merged_df['genres'].str.lower().str.contains(genre)]
-    
-    if not recommended_movies.empty:
-        st.subheader(f"Recommended {genre.capitalize()} Movies with Ratings")
-        st.write(recommended_movies)
-    else:
-        st.warning(f"No {genre.capitalize()} movies found. Try a different genre.")
+# Now you can use 'df' in your Streamlit app
