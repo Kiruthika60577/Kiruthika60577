cb75ae6b9239854fb5e4d0961726591f0ebf5c65
Kiruthika60577 <138123688+Kiruthika60577@users.noreply.github.com>
2023-09-06 12:44:42 +0200

    Update streamlitapp.py

--- streamlitapp.py
+++ streamlitapp.py
@@ -1,8 +1,4 @@
-import streamlit as st
-st.title ("wbsflix")
-st.header("movies recommendation")
-st.text("Enjoy the movies")
-st.text_input("userId")
+
 import streamlit as st
 import pandas as pd
 
