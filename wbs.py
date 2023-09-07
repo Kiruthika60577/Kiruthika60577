811d57abf7bf420ec7fadffd6187d977f87eef95
Kiruthika60577 <138123688+Kiruthika60577@users.noreply.github.com>
2023-09-07 11:52:55 +0200

    Update wbs.py

--- wbs.py
+++ wbs.py
@@ -1,6 +1,7 @@
 import streamlit as st
 import pandas as pd
-
+from surprise import Dataset, Reader, KNNBasic
+from surprise.model_selection import train_test_split
 
 # Upload your data files to your Streamlit app (e.g., using the "Upload File" widget)
 uploaded_movies = st.file_uploader("Upload movies.csv", type=["csv"])
@@ -16,15 +17,16 @@ if uploaded_movies and uploaded_ratings:
 
     # Sidebar for user input
     user_id = st.sidebar.text_input('Enter User ID', '1')
-
-    # Filter movies by genre
     selected_genre = st.sidebar.selectbox('Select Genre', movies_df['genres'].unique())
-    filtered_movies = movies_df[movies_df['genres'].str.contains(selected_genre)]
 
-    # ... (Rest of the code remains the same as in the previous response)
-    
+    # Button to trigger recommendations
+    if st.sidebar.button('Get Recommendations'):
+        # Filter movies by selected genre
+        filtered_movies = movies_df[movies_df['genres'].str.contains(selected_genre)]
+
+        # ... (Rest of the code for recommendations remains the same as in the previous response)
+        
 else:
     st.write("Please upload your data files.")
 
-# ... (Rest of the code remains the same as in the previous response)
-
+# ... (Rest of the code for recommendations remains the same as in the previous response)
