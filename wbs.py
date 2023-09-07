5ab356f680c8bf8a6d8c86cf41574fe38e95cdbc
Kiruthika60577 <138123688+Kiruthika60577@users.noreply.github.com>
2023-09-07 12:00:54 +0200

    Update wbs.py

--- wbs.py
+++ wbs.py
@@ -24,7 +24,7 @@ if uploaded_movies and uploaded_ratings:
         filtered_movies = movies_df[movies_df['genres'].str.contains(selected_genre)]
 
         # Load the recommendation model
-        data = Dataset.load_builtin('ml-100k')  # You can use the built-in MovieLens dataset
+       
         trainset, _ = train_test_split(data, test_size=0.2, random_state=42)
         algo = KNNBasic(sim_options={'name': 'cosine', 'user_based': True})
         algo.fit(trainset)
@@ -50,4 +50,4 @@ if uploaded_movies and uploaded_ratings:
 else:
     st.write("Please upload your data files.")
 
-# ... (Rest of the code remains the same as in the previous response)
+
