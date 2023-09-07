75fed5b84e910e65c10b929ac2bfdbaa995e1ec3
Kiruthika60577 <138123688+Kiruthika60577@users.noreply.github.com>
2023-09-07 11:50:18 +0200

    Update wbs.py

--- wbs.py
+++ wbs.py
@@ -7,12 +7,19 @@ uploaded_movies = st.file_uploader("Upload movies.csv", type=["csv"])
 uploaded_ratings = st.file_uploader("Upload ratings.csv", type=["csv"])
 
 # Streamlit App Header
-st.title('Movie Recommendation App')
+st.title('Wbsflix Movie Recommendation')
 
 # Load data
 if uploaded_movies and uploaded_ratings:
     movies_df = pd.read_csv(uploaded_movies)
     ratings_df = pd.read_csv(uploaded_ratings)
+
+    # Sidebar for user input
+    user_id = st.sidebar.text_input('Enter User ID', '1')
+
+    # Filter movies by genre
+    selected_genre = st.sidebar.selectbox('Select Genre', movies_df['genres'].unique())
+    filtered_movies = movies_df[movies_df['genres'].str.contains(selected_genre)]
 
     # ... (Rest of the code remains the same as in the previous response)
     
@@ -20,3 +27,4 @@ else:
     st.write("Please upload your data files.")
 
 # ... (Rest of the code remains the same as in the previous response)
+
