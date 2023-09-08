661a764c51f6d1cb5a597643a19b4eb90a18c67a
Kiruthika60577 <138123688+Kiruthika60577@users.noreply.github.com>
2023-09-08 08:51:51 +0200

    Update wbs.py

--- wbs.py
+++ wbs.py
@@ -19,4 +19,36 @@ background_style = f"""
 # Apply the custom CSS style
 st.markdown(background_style, unsafe_allow_html=True)
 
-# The rest of your Streamlit app code...
+import streamlit as st
+import pandas as pd
+
+# GitHub repository URL
+github_repo_url = 'https://raw.githubusercontent.com/Kiruthika60577/Kiruthika60577/main/'
+
+# URLs of your CSV files on GitHub
+movies_url = github_repo_url + 'movies.csv'
+ratings_url = github_repo_url + 'ratings.csv'
+
+# Streamlit App Header
+st.title('Wbsflix Movie Recommendation')
+
+# Load data from GitHub URLs
+movies_df = pd.read_csv(movies_url)
+ratings_df = pd.read_csv(ratings_url)
+import streamlit as st
+import pandas as pd
+
+# URLs of your CSV files on GitHub
+movies_url = 'https://raw.githubusercontent.com/Kiruthika60577/Kiruthika60577/main/movies.csv'
+ratings_url = 'https://raw.githubusercontent.com/Kiruthika60577/Kiruthika60577/main/ratings.csv'
+
+# Streamlit App Header
+st.title('Wbsflix Movie Recommendation')
+
+# Load data from GitHub URLs
+movies_df = pd.read_csv(movies_url)
+ratings_df = pd.read_csv(ratings_url)
+
+# Rest of your Streamlit app code...
+
+
