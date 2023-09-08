9e2b591ae66804d3025e18f3262c7d5ffadfadfb
Kiruthika60577 <138123688+Kiruthika60577@users.noreply.github.com>
2023-09-08 08:54:16 +0200

    Update wbs.py

--- wbs.py
+++ wbs.py
@@ -64,5 +64,42 @@ movies_df = pd.read_csv(movies_url)
 ratings_df = pd.read_csv(ratings_url)
 
 # Rest of your Streamlit app code...
+import streamlit as st
+import pandas as pd
+
+# URLs of your CSV files on GitHub
+movies_url = 'https://raw.githubusercontent.com/Kiruthika60577/Kiruthika60577/main/movies.csv'
+ratings_url = 'https://raw.githubusercontent.com/Kiruthika60577/Kiruthika60577/main/ratings.csv'
+
+# Streamlit App Header
+st.title('Wbsflix Movie Recommendation')
+
+# Load data from GitHub URLs
+movies_df = pd.read_csv(movies_url)
+ratings_df = pd.read_csv(ratings_url)
+
+# Sidebar for user input
+user_id = st.sidebar.text_input('Enter User ID', '1')
+selected_genre = st.sidebar.selectbox('Select Genre', movies_df['genres'].unique())
+
+# Search box for movie titles
+search_query = st.sidebar.text_input('Search for Movie', '')
+
+# Filter movies based on search query
+filtered_movies = movies_df[movies_df['title'].str.contains(search_query, case=False)]
+
+# Button to trigger recommendations
+if st.sidebar.button('Get Recommendations'):
+    # Filter movies by selected genre
+    filtered_movies = filtered_movies[filtered_movies['genres'].str.contains(selected_genre)]
+
+    # Load the recommendation model
+    # (Assuming you have already loaded and trained the model)
+
+    # Get movie recommendations for the user (similar to your previous code)
+
+# Display the filtered movies
+st.subheader('Filtered Movies:')
+st.dataframe(filtered_movies)
 
 
